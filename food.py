63ddb523d36299a3dd2fbad6b233ebac7ed2e135
Gab_km <the25thcromosome@gmail.com>
2012-04-15 21:24:12 +0900

    [from now] Sun Apr 15 21:24:13     2012
    diff --git a/food.py b/food.py
    index 3f48125..3b21cd3 100644
    --- a/food.py
    +++ b/food.py
    @@ -5,9 +5,22 @@ class Food:
         def __init__(self, name):
             self.__name = name
    
    +    def is_eaten_by(self, eater):
    +        pass
    +
     class Meat(Food):
    -    pass
    
    +    def is_eaten_by(self, eater):
    +        if isinstance(eater, Carnivore):
    +            return Taste(tasty=True)
    +        else:
    +            return Taste(tasty=False)
    +
     class Vegetable(Food):
    -    pass
    +
    +    def is_eaten_by(self, eater):
    +        if isinstance(eater, Vegetarian):
    +            return Taste(tasty=True)
    +        else:
    +            return Taste(tasty=False)

--- food.py
+++ food.py
@@ -5,9 +5,22 @@ class Food:
     def __init__(self, name):
         self.__name = name
 
+    def is_eaten_by(self, eater):
+        pass
+
 class Meat(Food):
-    pass
     
+    def is_eaten_by(self, eater):
+        if isinstance(eater, Carnivore):
+            return Taste(tasty=True)
+        else:
+            return Taste(tasty=False)
+
 class Vegetable(Food):
-    pass    
+    
+    def is_eaten_by(self, eater):
+        if isinstance(eater, Vegetarian):
+            return Taste(tasty=True)
+        else:
+            return Taste(tasty=False)
 
