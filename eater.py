60fc0a7925c805a4266c7c2273881d5e23c77282
Gab_km <the25thcromosome@gmail.com>
2012-04-15 20:33:40 +0900

    VegetarianがVegetableを食べてtastyなTasteを返すように実装
    プロダクトコードをeater.py、food.py、taste.pyに分割。
    Vegetarian<:EaterとなるようにVegetarianを定義。
    Vegetarian#eatでtasty=TrueであるTasteを返すように実装。
    
    diff --git a/food_and_eater.py b/food_and_eater.py
    deleted file mode 100644
    index 2708088..0000000
    --- a/food_and_eater.py
    +++ /dev/null
    @@ -1,33 +0,0 @@
    -# -*- coding: utf-8 -*-
    -
    -class Eater:
    -
    -    def eat(self, food):
    -        return Taste()
    -
    -class Carnivore(Eater):
    -
    -    def eat(self, food):
    -        if isinstance(food, Meat):
    -            return Taste(tasty=True)
    -        else:
    -            return Taste(tasty=False)
    -
    -class Food:
    -
    -    def __init__(self, name):
    -        self.__name = name
    -
    -class Meat(Food):
    -    pass
    -
    -class Vegetable(Food):
    -    pass
    -
    -class Taste:
    -
    -    def __init__(self, tasty=True):
    -        self.__tasty = tasty
    -
    -    def __eq__(self, other):
    -        return self.__tasty == other.__tasty
    diff --git a/test_food_and_eater.py b/test_food_and_eater.py
    index e731ce5..db21113 100644
    --- a/test_food_and_eater.py
    +++ b/test_food_and_eater.py
    @@ -1,8 +1,11 @@
     # -*- coding: utf-8 -*-
    
    -from food_and_eater import Eater, Food, Taste, Carnivore, Meat, Vegetable
    +from taste import Taste
    +from eater import Eater, Carnivore, Vegetarian
    +from food import Food, Meat, Vegetable
    
     def test():
         assert Eater().eat(Food("food")) == Taste()
         assert Carnivore().eat(Meat("Steak")) == Taste(tasty=True)
         assert Carnivore().eat(Vegetable("Salad")) == Taste(tasty=False)
    +    assert Vegetarian().eat(Vegetable("salad")) == Taste(tasty=True)

---
+++ eater.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from taste import Taste
+from food import Meat
+
+class Eater:
+    
+    def eat(self, food):
+        return Taste()
+
+class Carnivore(Eater):
+    
+    def eat(self, food):
+        if isinstance(food, Meat):
+            return Taste(tasty=True)
+        else:
+            return Taste(tasty=False)
+
+class Vegetarian(Eater):
+
+    def eat(self, food):
+        return Taste(tasty=True)
---
+++ food.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+class Food:
+
+    def __init__(self, name):
+        self.__name = name
+
+class Meat(Food):
+    pass
+    
+class Vegetable(Food):
+    pass    
+
