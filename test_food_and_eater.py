c98da81822e22eb25eb7995defd17fc9074ed048
Gab_km <the25thcromosome@gmail.com>
2012-04-15 20:05:35 +0900

    [from now] Sun Apr 15 20:05:35     2012
    diff --git a/test_food_and_eater.py b/test_food_and_eater.py
    index 43eb9ab..e731ce5 100644
    --- a/test_food_and_eater.py
    +++ b/test_food_and_eater.py
    @@ -3,6 +3,6 @@
     from food_and_eater import Eater, Food, Taste, Carnivore, Meat, Vegetable
    
     def test():
    -    assert Eater().eat(Food()) == Taste("food")
    +    assert Eater().eat(Food("food")) == Taste()
         assert Carnivore().eat(Meat("Steak")) == Taste(tasty=True)
         assert Carnivore().eat(Vegetable("Salad")) == Taste(tasty=False)

--- test_food_and_eater.py
+++ test_food_and_eater.py
@@ -3,6 +3,6 @@
 from food_and_eater import Eater, Food, Taste, Carnivore, Meat, Vegetable
 
 def test():
-    assert Eater().eat(Food()) == Taste("food")
+    assert Eater().eat(Food("food")) == Taste()
     assert Carnivore().eat(Meat("Steak")) == Taste(tasty=True)
     assert Carnivore().eat(Vegetable("Salad")) == Taste(tasty=False)
