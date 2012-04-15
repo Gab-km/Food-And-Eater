27229e833d7d9ca9efaedd71f06fc426aa287191
Gab_km <the25thcromosome@gmail.com>
2012-04-15 12:19:52 +0900

    EaterがFoodを食べてTasteを返すように実装
    EaterクラスにFoodクラスのインスタンスを引数に取るeatメソッドを追加。
    Tasteクラスに文字列として名前を取るコンストラクタを追加。
    
    diff --git a/test_food_and_eater.py b/test_food_and_eater.py
    new file mode 100644
    index 0000000..633f866
    --- /dev/null
    +++ b/test_food_and_eater.py
    @@ -0,0 +1,2 @@
    +# -*- coding: utf-8 -*-
    +

--- food_and_eater.py
+++ food_and_eater.py
@@ -1,2 +1,17 @@
 # -*- coding: utf-8 -*-
 
+class Eater:
+    
+    def eat(self, food):
+        return Taste("food")
+
+class Food:
+    pass
+
+class Taste:
+
+    def __init__(self, name):
+        self.__name = name
+
+    def __eq__(self, other):
+        return self.__name == other.__name
---
+++ test_food_and_eater.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from food_and_eater import Eater, Food, Taste
+
+def test():
+    assert Eater().eat(Food()) == Taste("food")
